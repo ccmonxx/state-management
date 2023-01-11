bdf5d68c33c4908e6593b8c040a371136d7b4877
chocomong <chocomonq@gmail.com>
2023-01-11 23:58:52 +0900

    Immutablity : 변경데이터만 교체한 배열을 반환하기

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -12,9 +12,16 @@ function ToDo({ text, category, id }: IToDo) {
 		// 1. 현재 선택한 toDo를 찾는 방법
 		setToDos((oldToDos) => {
 			const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-			const newToDo = { text, id, category: name };
-			// console.log(targetIndex, newToDo);
-			return oldToDos;
+
+			// 2. 카테고리가 변경된 toDo를 다시 반환하기
+			const newToDo = { id, text, category: name as any };
+			console.log(targetIndex); // >> index
+			console.log(newToDo); // >> {toDo}
+			return [
+				...oldToDos.slice(0, targetIndex), // 0~현재 타겟의 앞까지
+				newToDo,
+				...oldToDos.slice(targetIndex + 1), // 현재 타겟부터 끝까지
+			];
 		});
 	};
 
