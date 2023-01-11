275c6650cadbecc100e242d25079997b3e4857ef
chocomong <chocomonq@gmail.com>
2023-01-12 01:24:30 +0900

    Enums : 문자열타입의 카테고리 변수를 enum으로 보호하기

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
-import { useSetRecoilState } from "recoil";
-import { toDoState } from "../atoms";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { categoryState, toDoState } from "../atoms";
 
 interface IForm {
 	toDo: string;
@@ -8,11 +8,13 @@ interface IForm {
 
 function CreateToDo() {
 	const setToDos = useSetRecoilState(toDoState);
+	const category = useRecoilValue(categoryState);
+
 	const { register, handleSubmit, setValue } = useForm<IForm>();
 	const handleValid = ({ toDo }: IForm) => {
-		// console.log(data.toDo);
 		setToDos((oldToDos) => [
-			{ text: toDo, id: Date.now(), category: "TODO" },
+			// { text: toDo, id: Date.now(), category: category },
+			{ text: toDo, id: Date.now(), category },
 			...oldToDos,
 		]);
 		setValue("toDo", "");
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { categoryState, toDoSelector } from "../atoms";
+import { Categories, categoryState, toDoSelector } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -8,17 +8,19 @@ function ToDoList() {
 	const toDos = useRecoilValue(toDoSelector);
 	const [category, setCategory] = useRecoilState(categoryState);
 	const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-		setCategory(event.currentTarget.value);
+		setCategory(event.currentTarget.value as any);
 	};
+	console.log(toDos);
+
 	return (
 		<div>
 			<h1>To Dos</h1>
 			<hr />
 
 			<select value={category} onInput={onInput}>
-				<option value="TODO">TODO</option>
-				<option value="DOING">DOING</option>
-				<option value="DONE">DONE</option>
+				<option value={Categories.TODO}>TODO</option>
+				<option value={Categories.DOING}>DOING</option>
+				<option value={Categories.DONE}>DONE</option>
 			</select>
 
 			<CreateToDo />
